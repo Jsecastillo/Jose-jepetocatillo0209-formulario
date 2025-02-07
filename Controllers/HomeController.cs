e003ff60f5e09b7594b86fc925fe0b113d989e55
JOSE ANDRES CASTILLO PIZARRO <604870912@castrocarazo.ac.cr>
2025-02-07 15:55:01 -0600

    Cambio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using WebAppFormMVC.Data;
+using WebAppFormMVC.Models;
 using WebAppFormMVC.Models;
 
 namespace WebAppFormMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        public IActionResult Submit(ContactMessage model)
         {
-            return View();
-        }
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            _context.ContactMessages.Add(model);
+            _context.SaveChanges();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            TempData["SuccessMessage"] = "Tu mensaje ha sido enviado con éxito.";
+            return RedirectToAction("Index");
         }
     }
 }
