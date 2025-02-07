0c4975ea7c75177160e7fdb21c22cf804384a539
JOSE ANDRES CASTILLO PIZARRO <604870912@castrocarazo.ac.cr>
2025-02-07 15:54:50 -0600

    se agrega el metodo al controlador y la interfaz

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppFormMVC.Models; // Asegurate de importar el namespace correcto
+
+namespace WebAppFormMVC.Data
+{
+    public class ApplicationDbContext : DbContext // Falta heredar de DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<ContactMessage> ContactMessages { get; set; } // Corregí los nombres
+    }
+}
---
+++ Models/ContactMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppFormMVC.Models
+{
+    public class ContactMessage
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo electronico es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de correo no valido")]
+
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es obligatorio")]
+        [StringLength(500, ErrorMessage = "El mensaje no puede superar los  500 caracteres")]
+        public string Mensaje { get; set; }
+    }
+}
